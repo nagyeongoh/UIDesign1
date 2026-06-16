bf2631687694c1adaf103d1a26817b38fa0ddbc7
sanbidul-dotcom <sanbidul@snu.ac.kr>
2026-06-16 15:55:56 +0900

    Delete home_script.js

--- cookwithme/home_script.js
+++
@@ -1,7 +0,0 @@
-const pinkButtons = document.querySelectorAll(".pink-btn");
-
-pinkButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        button.classList.toggle("active");
-    });
-});
