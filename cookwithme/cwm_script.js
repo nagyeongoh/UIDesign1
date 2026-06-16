f937b9ba3649733c1b9a6c90004d30de237db896
sanbidul-dotcom <sanbidul@snu.ac.kr>
2026-06-16 15:56:02 +0900

    Delete cwm_script.js

--- cookwithme/cwm_script.js
+++
@@ -1,7 +0,0 @@
-const pinkButtons = document.querySelectorAll('.opt');
-
-pinkButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        button.classList.toggle('active');
-    });
-});
